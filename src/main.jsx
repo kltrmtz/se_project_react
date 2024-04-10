7866e2330d1ba3cffcabb8300354d1cb2cfd9bb2
Kolt Metz <kltrmtz@gmail.com>
2024-04-09 21:42:18 -0400

    Project14 script update

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./components/App.jsx";
-import reportWebVitals from "./reportWebVitals";
+import reportWebVitals from "./reportWebVitals.js";
 import { BrowserRouter } from "react-router-dom/cjs/react-router-dom.min.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
